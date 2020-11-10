0b5a6a014dd8f4ecb7d63972d463ea3d9c41fbe2
Vlad Shiltsin <vlad.shiltsin@gmail.com>
2020-11-10 14:27:43 +0200

    input.php: deleted old code

--- input.php
+++ input.php
@@ -1,31 +1,6 @@
 <?php
 
-function readFromConsole() : array
+function readFromConsole()
 {
-    echo "Введите числа, сумму которых хотите получить:\n";
 
-    $input = explode(" ", trim(fgets(STDIN)));
-    $result_input = [];
-
-    foreach($input as $value)
-    {
-        if (!is_numeric($value))
-        {
-            echo "Некорректные введенные данные\n";
-            readFromConsole();
-            break;
-        }
-
-        $result_input[] = (int)$value;
-    }
-
-    return $result_input;
-}
-
-function getSumOfElements()
-{
-    $result = array_sum(readFromConsole());
-
-    echo "Результат: {$result}";
-    return $result;
 }

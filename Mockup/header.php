16daad55fe8b57e5ff61b76d4086b3a01f910f8d
Vlad Shiltsin <vlad.shiltsin@gmail.com>
2020-11-17 17:08:29 +0200

    First half of site

---
+++ Mockup/header.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device.width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0,
+    minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="/Mockup/style.css">
+</head>
+<body>
+
+
+<div class="section first-section">
+    <div class="container">
+        <h1>Short headline</h1>
+        <p>This website is operated by the Design Files.
+            Throughout the site, the terms “we”, “us” and “our” refer to the Design Files.
+            The Design Files offers this website, including all information, tools,
+            and services available from this site to you, the user,
+            conditioned upon your acceptance of all terms, conditions, policies,
+            and notices stated here.</p>
+    </div>
+</div>
+
+<div class="section">
+    <div class="container">
+        <h2>Medium Length Title</h2>
+        <p>By creating an Account on our service, you agree to subscribe to newsletters,
+            marketing or promotional materials and other information we may send.
+            However, you may opt out of receiving any, or all,
+            of these communications from us by following the unsubscribe link or
+            instructions provided in any email we send.</p>
+        <h3>Medium length headline</h3>
+        <p class="notify">However, you may opt out of receiving any, or all,
+            of these communications from us by following the unsubscribe link or
+            instructions provided in any email we send.</p>
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>
+</div>
+
+
+<div class="section">
+
+</div>
+
+
+</body>
+</html>
+
+<?php
+
+

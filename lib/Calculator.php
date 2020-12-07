0025439ad1fadac6f0994c4671702484506e372a
Vlad Shiltsin <vlad.shiltsin@gmail.com>
2020-12-07 07:25:36 +0200

    Calculator 100% covered with tests

--- lib/Calculator.php
+++ lib/Calculator.php
@@ -3,28 +3,43 @@
 
 class Calculator
 {
-	public function add(int $a, int $b): int
-	{
-		return $a + $b;
-	}
+    public function add(int $a, int $b): int
+    {
+        return $a + $b;
+    }
 
-	public function subtract(int $a, int $b): int
-	{
-		return $a - $b;
-	}
+    public function subtract(int $a, int $b): int
+    {
+        return $a - $b;
+    }
 
-	public function multiply(int $a, int $b): int
-	{
-		return $a * $b;
-	}
+    public function multiply(int $a, int $b): int
+    {
+        return $a * $b;
+    }
 
-	public function divide(int $a, int $b): float
-	{
-		if($b === 0)
-		{
-			throw new InvalidArgumentException('Divider cant be a zero');
-		}
+    public function divide(int $a, int $b): float
+    {
+        if ($b === 0)
+        {
+            throw new InvalidArgumentException('Divider cant be a zero');
+        }
 
-		return $a / $b;
-	}
+        return $a / $b;
+    }
+
+    public function pow(int $base, int $exp): float
+    {
+        return $base**$exp;
+    }
+
+    public function sqrt(int $arg): float
+    {
+        if ($arg < 0)
+        {
+            throw new InvalidArgumentException("Argument can't be a negative number");
+        }
+
+        return sqrt($arg);
+    }
 }
--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -1,22 +1,71 @@
 <?php
 //CalculatorTest.php
 use \PHPUnit\Framework\TestCase;
-require_once (__DIR__ . '/../lib/Calculator.php');
+
+require_once(__DIR__.'/../lib/Calculator.php');
 
 class CalculatorTest extends TestCase
 {
-	public function testAdd(): void
-	{
-		$calculator = new Calculator();
-		self::assertEquals(4, $calculator->add(2, 2));
-	}
-
-	public function testDivide(): void
+    public function testAdd(): void
     {
-	    $calculator = new Calculator();
+        $calculator = new Calculator();
+        self::assertEquals(4, $calculator->add(2, 2));
+    }
 
-	    $this->expectException(InvalidArgumentException::class);
-	    $this->expectExceptionMessage("Divider cant be a zero");
-	    $calculator->divide(5,0);
+    public function testDivide(): void
+    {
+        $calculator = new Calculator();
+
+        self::assertEquals(4, $calculator->divide(8, 2));
+        self::assertEquals(-4, $calculator->divide(-8, 2));
+        self::assertEquals(0, $calculator->divide(0, 2534534));
+
+        self::expectException(InvalidArgumentException::class);
+        self::expectExceptionMessage("Divider cant be a zero");
+        $calculator->divide(5, 0);
+    }
+
+    public function testSubtract(): void
+    {
+        $calculator = new Calculator();
+
+        self::assertEquals(0, $calculator->subtract(2, 2));
+        self::assertEquals(4, $calculator->subtract(2, -2));
+        self::assertEquals(2333, $calculator->subtract(3333, 1000));
+    }
+
+    public function testMultiply(): void
+    {
+        $calculator = new Calculator();
+
+        self::assertEquals(0, $calculator->multiply(123237, 0));
+        self::assertEquals(4, $calculator->multiply(2, 2));
+    }
+
+    public function testPow(): void
+    {
+        $calculator = new Calculator();
+
+        self::assertEquals(1, $calculator->pow(5463437, 0));
+        self::assertEquals(0.25, $calculator->pow(2, -2));
+        self::assertEquals(27, $calculator->pow(3, 3));
+        self::assertEquals(1, $calculator->pow(1, 33));
+        self::assertEquals(4, $calculator->pow(-2, 2));
+        self::assertEquals(-8, $calculator->pow(-2, 3));
+        self::assertEquals(0, $calculator->pow(0, 3));
+    }
+
+    public function testSqrt(): void
+    {
+        $calculator = new Calculator();
+
+        self::assertEquals(0, $calculator->sqrt(0));
+        self::assertEquals(1, $calculator->sqrt(1));
+        self::assertEquals(4, $calculator->sqrt(16));
+        self::assertEquals(10, $calculator->sqrt(100));
+
+        self::expectException(InvalidArgumentException::class);
+        self::expectExceptionMessage("Argument can't be a negative number");
+        $calculator->sqrt(-1);
     }
 }

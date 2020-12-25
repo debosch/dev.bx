9a6afb77b859b3ed716c54d58b0579dcb5f5f38f
Vlad Shiltsin <vlad.shiltsin@gmail.com>
2020-12-25 22:59:29 +0200

    Product list page

--- moto-shop-layout/src/html/product-list-page.php
+++ moto-shop-layout/src/html/product-list-page.php
@@ -1,1 +1,117 @@
 <?php
+require_once 'moto-shop-layout/src/html/header.php';
+?>
+
+    <!doctype html>
+    <html lang="en">
+    <head>
+        <link rel="stylesheet" href="/moto-shop-layout/src/styles/home-page-style.css">
+        <link rel="stylesheet" href="/moto-shop-layout/src/styles/product-list-style.css">
+        <title>Product list</title>
+    <body class="body-custom">
+
+    <section class="presentation-section product-list-presentation-section">
+        <div class="container-fluid">
+            <div class="row custom-row">
+                <h4 class="presentation-header">Your Best Value Proposition</h4>
+                <div class="presentation-text">Lorem ipsum dolor sit amet,
+                    consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                    labore et dolore magna aliqua. Ut enim ad minim veniam,
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-4">
+                    <div class="card custom-card">
+                        <img src="/moto-shop-layout/src/img/image5.jpg" class="card-img-top card-img-action" alt="...">
+                        <div class="card-body">
+                            <p class="card-text card-text-custom">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                            <a href="/moto-shop-layout/src/html/detail-page.php" class="custom-link">Learn more</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="card custom-card">
+                        <img src="/moto-shop-layout/src/img/image7.png" class="card-img-top card-img-action" alt="...">
+                        <div class="card-body">
+                            <p class="card-text card-text-custom">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                            <a href="/moto-shop-layout/src/html/detail-page.php" class="custom-link">Learn more</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="card custom-card">
+                        <img src="/moto-shop-layout/src/img/image6.png" class="card-img card-img-action" alt="...">
+                        <div class="card-body">
+                            <p class="card-text card-text-custom">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                            <a href="/moto-shop-layout/src/html/detail-page.php" class="custom-link">Learn more</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-4">
+                    <div class="card custom-card">
+                        <img src="/moto-shop-layout/src/img/image5.jpg" class="card-img-top card-img-action" alt="...">
+                        <div class="card-body">
+                            <p class="card-text card-text-custom">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                            <a href="/moto-shop-layout/src/html/detail-page.php" class="custom-link">Learn more</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="card custom-card">
+                        <img src="/moto-shop-layout/src/img/image7.png" class="card-img-top card-img-action" alt="...">
+                        <div class="card-body">
+                            <p class="card-text card-text-custom">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                            <a href="/moto-shop-layout/src/html/detail-page.php" class="custom-link">Learn more</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="card custom-card">
+                        <img src="/moto-shop-layout/src/img/image6.png" class="card-img card-img-action" alt="...">
+                        <div class="card-body">
+                            <p class="card-text card-text-custom">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                            <a href="/moto-shop-layout/src/html/detail-page.php" class="custom-link">Learn more</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-4">
+                    <div class="card custom-card">
+                        <img src="/moto-shop-layout/src/img/image5.jpg" class="card-img-top card-img-action" alt="...">
+                        <div class="card-body">
+                            <p class="card-text card-text-custom">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                            <a href="/moto-shop-layout/src/html/detail-page.php" class="custom-link">Learn more</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="card custom-card">
+                        <img src="/moto-shop-layout/src/img/image7.png" class="card-img-top card-img-action" alt="...">
+                        <div class="card-body">
+                            <p class="card-text card-text-custom">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                            <a href="/moto-shop-layout/src/html/detail-page.php" class="custom-link">Learn more</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="card custom-card">
+                        <img src="/moto-shop-layout/src/img/image6.png" class="card-img card-img-action" alt="...">
+                        <div class="card-body">
+                            <p class="card-text card-text-custom">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                            <a href="/moto-shop-layout/src/html/detail-page.php" class="custom-link">Learn more</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+    </section>
+
+    </body>
+    </html>
+
+<?php
+require_once 'moto-shop-layout/src/html/footer.php';
+?>

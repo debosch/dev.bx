ed79d6ec54bedecba2b71635df85d2d96d93597c
Vlad Shiltsin <vlad.shiltsin@gmail.com>
2020-12-25 22:02:08 +0200

    Main page

--- moto-shop-layout/src/html/header.php
+++ moto-shop-layout/src/html/header.php
@@ -3,15 +3,8 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
-          integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link rel="stylesheet" href="/moto-shop-layout/src/styles/style.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="/moto-shop-layout/src/styles/header.css">
+    <title>Header</title>
 </head>
 <body>
 
@@ -20,10 +13,10 @@
             <div class="row align-items-center">
                 <div class="col">
                     <div class="row header-links">
-                        <a href="" class="col-lg-3 col-md-6 nav-link text-nowrap">Link 1</a>
-                        <a href="" class="col-lg-3 col-md-6 nav-link text-nowrap">Link 2</a>
-                        <a href="" class="col-lg-3 col-md-6 nav-link text-nowrap">Link 3</a>
-                        <a href="" class="col-lg-3 col-md-6 nav-link text-nowrap">Link 4</a>
+                        <a href="" class="col-lg-3 col-md-6 nav-link text-nowrap header-link">Link 1</a>
+                        <a href="" class="col-lg-3 col-md-6 nav-link text-nowrap header-link">Link 2</a>
+                        <a href="" class="col-lg-3 col-md-6 nav-link text-nowrap header-link">Link 3</a>
+                        <a href="" class="col-lg-3 col-md-6 nav-link text-nowrap header-link">Link 4</a>
                     </div>
                 </div>
                 <div class="col">
--- moto-shop-layout/src/html/home-page.php
+++ moto-shop-layout/src/html/home-page.php
@@ -1,15 +1,12 @@
-<?php ?>
+<?php
+ require_once 'moto-shop-layout/src/html/header.php';
+?>
 
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
-          integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link rel="stylesheet" href="/moto-shop-layout/src/styles/style.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="/moto-shop-layout/src/styles/home-page-style.css">
+    <title>Home page</title>
 </head>
 <body>
 
@@ -84,6 +81,9 @@
         </div>
     </section>
 
-
 </body>
 </html>
+
+<?php
+require_once 'moto-shop-layout/src/html/footer.php';
+?>

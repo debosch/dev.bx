322cc9e0b4b0c49b71901a58a6be031d1e2e4643
Vlad Shiltsin <vlad.shiltsin@gmail.com>
2020-12-26 05:18:01 +0200

    Login page

--- moto-shop-layout/src/html/login-page.php
+++ moto-shop-layout/src/html/login-page.php
@@ -1,1 +1,39 @@
 <?php
+require_once 'moto-shop-layout/src/html/header.php';
+?>
+
+    <!doctype html>
+    <html lang="en">
+    <head>
+        <link rel="stylesheet" href="/moto-shop-layout/src/styles/login-page-style.css">
+        <title>Document</title>
+    </head>
+    <body>
+
+    <div class="container-fluid">
+        <div class="wrapper">
+            <div class="login-wrapper">
+                <div class="row">
+                    <div class="col">
+                        <p class="login-label">Try the product out for free.</p>
+                        <form>
+                            <div class="mb-3">
+                                <input type="email" class="form-control login-input black-border" placeholder="email" id="inputEmail1" aria-label="email">
+                            </div>
+                            <div class="mb-3">
+                                <input type="password" class="form-control login-input black-border" placeholder="password" id="inputPassword1" aria-label="password">
+                            </div>
+                            <button type="submit" class="btn btn-start-trial-text btn-dark btn-start-trial text-center">Start free trial</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    </body>
+    </html>
+
+<?php
+require_once 'moto-shop-layout/src/html/footer.php';
+?>

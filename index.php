a8be719d6dbc06529dd170f4d5adf750c9e65e7e
Vlad Shiltsin <vlad.shiltsin@gmail.com>
2020-12-26 06:39:02 +0200

    index.php with bootstrap added

--- index.php
+++ index.php
@@ -1,6 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+          integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+</html>
 <?php
-
-require_once 'moto-shop-layout/src/html/header.php';
 require_once 'moto-shop-layout/src/html/home-page.php';
-require_once 'moto-shop-layout/src/html/footer.php';
+//require_once 'moto-shop-layout/src/html/detail-page.php';
+//require_once 'moto-shop-layout/src/html/product-list-page.php';
+//require_once 'moto-shop-layout/src/html/login-page.php';
+
 
